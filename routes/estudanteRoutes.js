5179818861f8c83d9a6d76e79a92d4973ed04c8a
jtiagotoledo <99657383+jtiagotoledo@users.noreply.github.com>
2026-08-12 18:44:35 -0300

    ajuste rota exclusao tarefa

--- routes/estudanteRoutes.js
+++ routes/estudanteRoutes.js
@@ -6,7 +6,9 @@ const uploadFotosMiddleware = require('../middlewares/upload');
 router.post('/login', estudanteController.obterPainelEstudante);
 router.post('/entregas', uploadFotosMiddleware, estudanteController.enviarTarefa);
 router.post('/painel', estudanteController.obterPainelEstudante);
-router.put('/entregar', estudanteController.atualizarEntrega);  
+router.put('/entregar', estudanteController.atualizarEntrega);
+
 router.delete('/entregar', estudanteController.excluirEntrega);
+router.delete('/entregas', estudanteController.excluirEntrega);
 
 module.exports = router;
